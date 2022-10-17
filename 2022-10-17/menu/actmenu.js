b328ebba28a903c8bdc7021c29d45c80b5083624
Your Name <you@example.com>
2022-10-17 12:42:48 +0200

    kesz

---
+++ 2022-10-17/menu/actmenu.js
@@ -0,0 +1,8 @@
+let linksArr=document.links
+for(let item of linksArr){
+    console.log('tömb:',obj.href)
+    console.log('URL:',document.URL)
+    for(obj.href==document.URL){
+        obj.style.color="red"
+    }
+}
