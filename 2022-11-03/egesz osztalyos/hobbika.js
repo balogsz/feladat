22e4de0c98d3fde5aebf95f983c41f20dd2c9d78
Your Name <you@example.com>
2022-11-03 10:36:43 +0100

    2022-11-03

---
+++ 2022-11-03/egesz osztalyos/hobbika.js
@@ -0,0 +1,9 @@
+const sorozat=[
+    {"id":"1","nev":"Dexter","kiadaseve":"2006","evadok":"8","kategoria":"pszichológiai thriller"},
+    {"id":"2","nev":"Game of Thrones","kiadaseve":"2011","evadok":"8","kategoria":"fantasy"},
+    {"id":"3","nev":"Shameless","kiadaseve":"2011","evadok":"11","kategoria":"dráma"},
+    {"id":"4","nev":"One of Us Is Lying","kiadaseve":"2021","evadok":"2","kategoria":"tinédzserdráma"},
+    {"id":"5","nev":"Stranger Things","kiadaseve":"2016","evadok":"4","kategoria":"misztikus"},
+    {"id":"6","nev":"Szörnyeteg: A Jeffrey Dahmer-sztori","kiadaseve":"2022","evadok":"1","kategoria":"thriller"},
+    {"id":"7","nev":"A kígyó","kiadaseve":"2021","evadok":"1","kategoria":"krimi"},
+]
